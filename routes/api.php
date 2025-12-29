4dd8f722caba80d0acb4d55f66c1d8a0578324c2
rafidhn06 <rafidhn06@gmail.com>
2025-12-29 21:26:32 +0700

    feat(auth): add account deletion and stricter password validation

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::delete('/auth/account', [AuthController::class, 'destroy']);
     Route::get('/user', function (\Illuminate\Http\Request $request) {
         return $request->user();
     });
