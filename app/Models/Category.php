a85276d32f5fc41fa47ac0ad38809be1f532f793
rafidhn06 <rafidhn06@gmail.com>
2025-12-31 01:43:23 +0700

    refactor(category): replace slug payload with icons

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'icon'];
+    protected $fillable = ['name', 'icon'];
 
     public function locations()
     {
